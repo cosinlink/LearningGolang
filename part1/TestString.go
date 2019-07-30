dc328b0d12ed63087d96ac22d8510f6eb5a0f390
fendouhyz <fendouhyz@163.com>
2019-07-30 15:39:55 +0800

    add some func

---
+++ part1/TestString.go
@@ -0,0 +1,16 @@
+package part1
+
+import "fmt"
+
+func TestStringNextLine() {
+	str := `
+	ldsjfalj
+asdjfklasdjf
+dfjal;sdjf 
+df;sadkl;lasdkf
+\\fsd\fdsf][sd[fasdf/ffsdfjklsdfkl;sadf
+&342462378%￥%@#￥！@#F？GVSDFGtex？dfgfgdfkg;dflk
+fdgksdf;；苏打粉
+`
+	fmt.Println(str)
+}
