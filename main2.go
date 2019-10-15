d7784a93ac024a6acecf29e7c07756e92c17b5af
fendouhyz <fendouhyz@163.com>
2019-10-15 17:17:04 +0800

    add windows_test

--- main2.go
+++ main2.go
@@ -1,4 +1,4 @@
-package main
+﻿package main
 
 import (
 	"fmt"
@@ -41,7 +41,7 @@ func func_log2file() {
 
 func func_log2fileAndStdout() {
 	//创建日志文件
-	f, err := os.OpenFile("test.log", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+	f, err := os.OpenFile("test_windows.log", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
 	if err != nil {
 		log.Fatal(err)
 	}
